21796f5556110a9710ba21f08c75570a839b55de
frontierpsycho <unfortunate42@gmail.com>
2014-09-01 17:13:15 +0200

    Changed to bar chart.

--- js/rechart.js
+++ js/rechart.js
@@ -6,61 +6,73 @@ $(function() {
   $.jqplot.config.enablePlugins = true;
  
   var plot;
-  var data = [['Poor', 1],['Rich', 9]];
+  var data = [[1], [9]];
   var options = {
-    title: 'Highlighting, Dragging, Cursor and Trend Line',
+    title: 'Inequality',
+    stackSeries: true,
     axesDefaults: {
       min: 0,
       max: 10
     },
-     axes: {
-         xaxis: {
-             renderer: $.jqplot.CategoryAxisRenderer,
-             tickOptions: {
-                 formatString: '%.2f'
-             }
-         },
-         yaxis: {
-             tickOptions: {
-                 formatString: '%.2f'
-             }
-         }
-     },
-     highlighter: {
-         sizeAdjust: 10,
-         tooltipLocation: 'sw',
-         tooltipAxes: 'both',
-         tooltipFormatString: '%.2f',
-         useAxesFormatters: false
-     },
-     cursor: {
-         show: true
-     },
-     series: [{
-       dragable: {
-         constrainTo: 'y'
-       }
-     }]
+    axes: {
+      xaxis: {
+        renderer: $.jqplot.CategoryAxisRenderer,
+        tickOptions: {
+          formatString: '%.2f'
+        }
+      },
+      yaxis: {
+        tickOptions: {
+          formatString: '%.2f'
+        }
+      }
+    },
+    seriesDefaults:{
+      renderer:$.jqplot.BarRenderer,
+      rendererOptions: {
+        highlightMouseDown: true
+      },
+      pointLabels: {show: true}
+    },
+    legend: {
+      show: true,
+      location: 'e',
+      placement: 'outside'
+    },
+    highlighter: {
+      sizeAdjust: 10,
+      tooltipLocation: 'sw',
+      tooltipAxes: 'both',
+      tooltipFormatString: '%.2f',
+      useAxesFormatters: false
+    },
+    cursor: {
+      show: true
+    },
+    series: [{
+      dragable: {
+        constrainTo: 'y'
+      }
+    }]
   };
 
   var replot = function() {
     if (plot) {
       console.debug('Replotting:', data);
-      plot.replot({ data: [data], resetAxes: false, clear: false });
+      plot.replot({ data: data, resetAxes: false });
     } else {
-      plot = $.jqplot('chartdiv', [data], options);
+      plot = $.jqplot('chartdiv', data, options);
     }
   };
 
   replot();
 
   var dragStopHandler = function(event, newPos, newData) { 
-    var delta = newData.yaxis - data[1][1];
-    console.debug('Delta:', newData, data, delta);
+    var delta = (newData.yaxis - data[0][0]);
+    console.debug('Delta:', delta);
 
-    data[0][1] -= delta;
-    data[1][1] += delta;
-    console.debug(data);
+    data[0][0] += delta;
+    data[1][0] -= delta;
 
     replot();
   };
